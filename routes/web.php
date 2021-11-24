b2a439848c581e4402b51e68ad96926718270c16
julisondev <julioalfredoalbino@gmail.com>
2021-11-24 11:19:11 +0000

    Configuração do template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function(){
+
+    Route::get('/', [AuthController::class, 'showLoginform'])->name('login');
 });
